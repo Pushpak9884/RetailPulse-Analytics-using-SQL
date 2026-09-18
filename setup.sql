64d0eecf7d4ccc2e483e1030abc08e11c3e04296
PUSHPAK <muppiripushpak2006@gmail.com>
2026-09-18 16:45:17 +0530

    Update setup.sql

--- setup.sql
+++ setup.sql
@@ -1,1 +1,64 @@
+CREATE DATABASE IF NOT EXISTS retailpulse;
+USE retailpulse;
 
+CREATE TABLE customers (
+    customer_id INT PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    email VARCHAR(100) UNIQUE,
+    city VARCHAR(50),
+    state VARCHAR(50),
+    registration_date DATE
+);
+
+CREATE TABLE products (
+    product_id INT PRIMARY KEY,
+    product_name VARCHAR(100),
+    category VARCHAR(50),
+    unit_price DECIMAL(10,2)
+);
+
+CREATE TABLE orders (
+    order_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_id INT,
+    order_date DATETIME,
+    order_status VARCHAR(30),
+    shipping_city VARCHAR(50),
+    sales_channel VARCHAR(30)
+);
+
+CREATE TABLE order_items (
+    order_item_id INT AUTO_INCREMENT PRIMARY KEY,
+    order_id INT,
+    product_id INT,
+    quantity INT,
+    unit_price DECIMAL(10,2),
+    discount DECIMAL(5,2)
+);
+
+CREATE TABLE payments (
+    payment_id INT AUTO_INCREMENT PRIMARY KEY,
+    order_id INT,
+    payment_date DATETIME,
+    payment_method VARCHAR(30),
+    payment_status VARCHAR(30),
+    amount DECIMAL(12,2)
+);
+
+CREATE TABLE returns (
+    return_id INT AUTO_INCREMENT PRIMARY KEY,
+    order_id INT,
+    product_id INT,
+    return_date DATE,
+    return_quantity INT,
+    return_reason VARCHAR(100)
+);
+
+CREATE TABLE inventory (
+    inventory_id INT AUTO_INCREMENT PRIMARY KEY,
+    product_id INT,
+    warehouse VARCHAR(50),
+    stock_quantity INT,
+    reorder_level INT,
+    last_restock_date DATE
+);
